8c6dd3818314dd9d6084f1abf9fd4e1acaef93e0
Gleb Shkut <gleb.shkut@gmail.com>
2023-07-20 12:52:05 -0700

    delete selectors

--- components/selectors/getUserId.ts
+++
@@ -1,5 +0,0 @@
-import { StoreSchema } from "@/components/store/store";
-import { DeepPartial } from "@reduxjs/toolkit";
-
-export const getUserId = (state: DeepPartial<StoreSchema>) =>
-  state?.user?.authData?.id;
--- components/selectors/getUserRole.ts
+++
@@ -1,5 +0,0 @@
-import { StoreSchema } from "@/components/store/store";
-import { DeepPartial } from "@reduxjs/toolkit";
-
-export const getUserRole = (state: DeepPartial<StoreSchema>) =>
-  state?.user?.authData?.role;
